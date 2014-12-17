bbfda6fe0dea48030a03a834555bafc46a084ccc
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-12-17 17:27:20 +0000

    handlebars engine

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var express = require('express'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     methodOverride = require('method-override'),
+    expresshbs = require('express-handlebars'),
     passport = require('passport'),
     TwitterStrategy = require('passport-twitter'),
     FacebookStrategy = require('passport-facebook'),
@@ -30,13 +31,11 @@ var app = express();
 //=======================EXPRESS================================
 // CONFIGURE EXPRESS
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 // Middleware
 app.use(favicon()); // finding of a URL of a web site's favicon
-app.use(logger('dev')); // why format common & combined do not work?
+app.use(logger('dev')); // HTTP request logger middleware  ? why format "common" & "combined" do not work ?
 app.use(cookieParser()); // parsing Cookie header and populate req.cookies with an object keyed by the cookie names
 app.use(bodyParser.urlencoded({ extended: false})); // return middleware that only parses urlencoded bodies
 app.use(bodyParser.json()); // return middleware that only parses JSON
@@ -45,12 +44,59 @@ app.use(session({ secret: 'userauth', saveUninitialized: true, resave: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static(path.join(__dirname, 'public')));
+// Session persisted message middleware
+app.use(function(req,res,next){
+    var err = req.session.error,
+        msg = req.session.notice,
+        success = req.session.success;
+
+    delete req.session.error;
+    delete req.session.notice;
+    delete req.session.success;
+
+    if (err) {
+        res.locals.error = err;
+    }
+
+    if (msg) {
+        res.locals.notice = msg;
+    }
+
+    if (success) {
+        res.locals.success = success;
+    }
+
+    next();
+});
+
+
+
+//===========================VIEWS,LAYOUTS ETC==================
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Configure express to use handlebars templates
+var hbs = expresshbs.create({
+    defaultLayout: 'main'
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 //===================Routes======================================
 
-app.use('/', routes);
-app.use('/users', users);
+//displays homepage
+app.get('/', function(req, res){
+  res.render('home', {user: req.user});
+});
+
+//displays signup page
+app.get('/signin', function(req, res){
+  res.render('signin');
+});
 
 
 //==================ERROR HANDLERS===============================
