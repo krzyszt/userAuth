b814285f18d8457d0229c579b3ed2e41a2977dc9
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2014-12-22 11:50:26 +0000

    mongoose added

--- config/authenticate.js
+++ config/authenticate.js
@@ -1,47 +1,18 @@
-var bcrypt = require('bcrypt-nodejs'),
-	Q = require('q'),
-	config = require('./config'),
-	db = require('orchestrate')(config.db);
+var bcrypt = require('bcrypt-nodejs');
+	
+	
 
 exports.localRegister = function(req, username, password){
-	var deferred = Q.defer();
 	var hash = bcrypt.hashSync(password);
-	var user = {
+    var user = {
 		"username": username,
 		"password": hash,
 		"email": username,
 		"firstName": req.body.firstName,
 		"lastName": req.body.lastName
 	}
-	console.log("New user");
-	console.log(user);
-
-	db.get('local-users', username)
-		.then(function(result){
-			console.log('username: ' + username + ' already exists');
-			deferred.resolve(false);
-		})
-		.fail(function(result){
-			console.log(result.body);
-			if (result.body.message == "The requested items could not be found."){
-				console.log('Username: ' + username + ' is free to use');
-				db.put('local-users', username, user)
-					.then(function(){
-						console.log('User: ' + user);
-						deferred.resolve(user);
-					})
-					.fail(function(err){
-						console.log('PUT FAIL: ' + err.body);
-						deferred.reject( new Error(err.body));
-					});
-			} else {
-				deferred.reject(new Error(result.body));
-			}
-		});
-
-	return deferred.promise;
 };
 
 exports.localAuth = function(username, password){
-	var deferred = Q.defer();
+	
 };
--- config/config.js
+++ config/config.js
@@ -1,3 +1,27 @@
-module.exports = {
-	"db": "MY_ORCHESTRATE_API_KEY"
-}
+//===================== MongoDB config =========================================
+var mongoose = require('mongoose');
+var dbURI = "mongodb://localhost:userAuth";
+
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function(){
+   console.log('Mongoose default connection open to ' + dbURI);
+});
+
+mongoose.connection.on('error', function(err){
+   console.log('Mongoose default connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function(){
+   console.log('Mongoose default connection disconnected');
+});
+
+process.on('SIGIN', function(){
+   mongoose.connection.close(function(){
+      console.log('Mongoose default connection disconnected through app termination');
+      process.exit(0);
+   });
+});
+
+//====================== Mongoose Models =======================================
+
